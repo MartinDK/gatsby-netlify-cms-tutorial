ab25ed8d2ff89a1834fcf61fdd422147b956bdbf
Martin D K <mtin727@gmail.com>
2019-11-26 16:16:34 +1100

    Deleted website icon and renamed site

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/github-icon.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -41,8 +40,7 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">MK
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <Link to="/" className="navbar-item" title="Logo">martindk
             </Link>
             {/* Hamburger menu */}
             <div
