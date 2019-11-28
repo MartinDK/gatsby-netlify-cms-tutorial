cbd7c48a01697dfa7fedd0b69a0e8dbbc58b6945
Martin Daniel <mtin722@gmail.com>
2019-11-28 12:22:57 +1100

    Fixed todos link in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,8 +61,8 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link className="navbar-item" to="/todos">
+                Todos
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
